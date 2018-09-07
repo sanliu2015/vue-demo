fb79259d44f7c5731ecc340c9f01cde80e07e379
sanliu2015 <717208317@qq.com>
2018-09-07 18:08:05 +0800

    1

--- src/page/exam1.vue
+++ src/page/exam1.vue
@@ -1,187 +1,230 @@
 <template>
   <div class="exam">
-      <div class="header">
-          <p class="title">考试试题</p>
-          <p class="describe">
-              <span>考试日期: {{currentDate}}</span>
-              <span>时长: {{time}}分钟</span>
-          </p>
+    <div class="header">
+      <p class="title">考试试题</p>
+      <p class="describe">
+        <span>考试日期: {{currentDate}}</span>
+        <span>时长: {{time}}分钟</span>
+      </p>
+    </div>
+    <section v-for="(item, index) in examList" v-show="index === currendIndex">
+      <label>{{ index + 1 }}. <span>{{item.title}} ({{item.score}}分)</span></label>
+      <div class="item-options" v-if="Array.isArray(item.answer)">
+        <CheckboxGroup v-model="checkedValue" @on-change="changeCheckboxAnswer">
+          <Checkbox v-for="(option, listIndex) in item.options" :label="listIndex+1" :key="listIndex"><span v-html="option"></span></Checkbox>
+        </CheckboxGroup>
       </div>
-      <section v-for="(item, index) in examList" v-show="index == currendIndex">
-          <label>{{ index + 1 }}. <span>{{item.title}} ({{item.score}}分)</span></label>
-          <div class="item-options">
-              <RadioGroup v-model="selectedValue" @on-change="changeCurrentAnswer">
-                  <Radio v-for="(option, listIndex) in item.options" :label="listIndex+1" :key="listIndex"><span v-html="option"></span></Radio>
-              </RadioGroup>
-          </div>
-      </section>
-      <div class="time-count-wrap">距离考试结束还有：{{restTime}}</div>
-      <div class="buttons-group">
-          <Button type="error" size="large" @click.native="prevItem" :disabled="currendIndex<1 && currendIndex != examList.length-1">上一题</Button>
-          <Button type="error" size="large" @click.native="nextItem" v-if="currendIndex != examList.length-1">下一题</Button>
-          <Button type="error" size="large" @click.native="handleSubmit" v-if="currendIndex == examList.length-1">提交试卷</Button>
+      <div v-else class="item-options">
+        <RadioGroup v-model="selectedValue" @on-change="changeCurrentAnswer">
+          <Radio v-for="(option, listIndex) in item.options" :label="listIndex+1" :key="listIndex"><span v-html="option"></span></Radio>
+        </RadioGroup>
       </div>
+    </section>
+    <div class="time-count-wrap">距离考试结束还有：{{restTime}}</div>
+    <div class="buttons-group">
+      <Button type="error" size="large" @click.native="prevItem" :disabled="currendIndex<1 && currendIndex != examList.length-1">上一题</Button>
+      <Button type="error" size="large" @click.native="nextItem" v-if="currendIndex != examList.length-1">下一题</Button>
+      <Button type="error" size="large" @click.native="handleSubmit" v-if="currendIndex == examList.length-1">提交试卷</Button>
+    </div>
   </div>
 </template>
 
 <script>
-import {mapState, mapMutations} from 'vuex'
-import {getNumberPrefix, getCurrentDate} from '../utils/common'
+  import {mapState, mapMutations} from 'vuex'
+  import {getNumberPrefix, getCurrentDate} from '../utils/common'
 
-export default {
+  export default {
     data () {
-        return {
-            currentDate: getCurrentDate(),
-            //选中的答案
-            selectedValue: "",
-            //初始化时间戳
-            currentTime: new Date().getTime(),
-            restTime: "",
-            timer: ""
-        }
+      return {
+        currentDate: getCurrentDate(),
+        selectedValue: "", //选中的答案
+        checkedValue: [], // 多选题选择的答案
+        //初始化时间戳
+        currentTime: new Date().getTime(),
+        restTime: "",
+        timer: ""
+      }
     },
     watch: {
       $route (to) {
       }
     },
     mounted () {
-        this.countTime();
-        // console.log(this.examList)
+      this.countTime();
+      // console.log(this.examList)
     },
     computed: {
-        examList() {
-            const vm = this;
-            return vm.$store.state.examList[vm.$route.query.id - 1];
-        },
-        time() {
-            const vm = this;
-            return vm.$store.state.time[vm.$route.query.id - 1];
-        },
-        ...mapState([
-            'currendIndex',
-            'saveAnswer'
-        ])
+      examList() {
+        const vm = this;
+        return vm.$store.state.examList[vm.$route.query.id - 1];
+      },
+      time() {
+        const vm = this;
+        return vm.$store.state.time[vm.$route.query.id - 1];
+      },
+      ...mapState([
+        'currendIndex',
+        'saveAnswer'
+      ])
     },
     methods: {
-        ...mapMutations([
-            'NEXT_ITEM',
-            'PREV_ITEM',
-            'USE_TIME',
-            'EXAM_ID'
-        ]),
-        //下一题
-        nextItem() {
-            if(!this.selectedValue){
-                this.$Message.warning('请先选择答案！');
-                return;
-            }
-            this.selectedValue = "";
-            this.NEXT_ITEM();
-            this.getCurrentAnswer();
-        },
-        //上一题
-        prevItem() {
-            this.PREV_ITEM();
-            this.getCurrentAnswer();
-        },
-        //提交答案
-        handleSubmit() {
-            this.EXAM_ID(this.$route.query.id);
-            this.USE_TIME(new Date().getTime() - this.currentTime);
-            clearTimeout(this.timer);
-            this.$router.push({name: 'Result'});
-        },
-        //更改当前题目的答案
-        changeCurrentAnswer() {
-            this.saveAnswer[this.currendIndex] = this.selectedValue;
-        },
-        //获取当前题目填写的答案
-        getCurrentAnswer() {
-            const currentAnswer = this.saveAnswer[this.currendIndex];
-            if(currentAnswer){
-                this.selectedValue = currentAnswer;
-            }else{
-                this.saveAnswer[this.currendIndex] = this.selectedValue;
-            }
-            console.log(this.saveAnswer);
-        },
-        //倒计时
-        countTime() {
-            const vm = this,
-                  endTime = vm.time * 60 * 1000 + vm.currentTime,
-                  currentTime = new Date().getTime(),
-                  restTime = endTime - currentTime,
-                  hours = getNumberPrefix(parseInt(restTime / (1000 * 60 * 60) % 24, 10)),
-                  minutes = getNumberPrefix(parseInt(restTime / (1000 * 60) % 60, 10)),
-                  seconds = getNumberPrefix(parseInt(restTime / 1000 % 60, 10));
-            vm.restTime = `${hours}:${minutes}:${seconds}`;
-            vm.timer = setTimeout(function(){
-                if(restTime > 0){
-                    vm.countTime();
-                }else if(restTime <= 0){
-                    clearTimeout(vm.timer);
-                    vm.$Message.warning("交卷时间已到，系统将帮您自动交卷");
-                    setTimeout(() => {
-                      vm.handleSubmit();
-                    }, 2000)
-                }
-            }, 1000);
+      ...mapMutations([
+        'NEXT_ITEM',
+        'PREV_ITEM',
+        'USE_TIME',
+        'EXAM_ID'
+      ]),
+      //下一题
+      nextItem() {
+        if(!this.selectedValue && this.checkedValue.length === 0){
+          this.$Message.warning('请先选择答案！');
+          return;
         }
+        this.selectedValue = "";
+        this.checkedValue = [];
+        this.NEXT_ITEM();
+        this.getCurrentAnswer();
+      },
+      //上一题
+      prevItem() {
+        this.PREV_ITEM();
+        this.getCurrentAnswer();
+      },
+      //提交答案
+      handleSubmit() {
+        if(!this.selectedValue && this.checkedValue.length === 0){
+          this.$Message.warning('请先选择答案！');
+          return;
+        }
+
+        this.$Modal.confirm({
+          title: '提示',
+          content: '<p>确定要交卷吗？</p>',
+          onOk: () => {
+            this.$Message.info('Clicked ok');
+          },
+          onCancel: () => {
+            this.$Message.info('Clicked cancel');
+          }
+        });
+
+
+
+        this.EXAM_ID(this.$route.query.id);
+        this.USE_TIME(new Date().getTime() - this.currentTime);
+        clearTimeout(this.timer);
+        // this.$router.push({name: 'Result'});
+      },
+      //更改当前题目的答案
+      changeCurrentAnswer() {
+        this.saveAnswer[this.currendIndex] = this.selectedValue;
+      },
+      changeCheckboxAnswer() {
+        this.saveAnswer[this.currendIndex] = this.checkedValue;
+      },
+      //获取当前题目填写的答案
+      getCurrentAnswer() {
+        const currentAnswer = this.saveAnswer[this.currendIndex];
+        if (currentAnswer !== undefined) {
+          if (Array.isArray(currentAnswer)) {
+            this.checkedValue = currentAnswer;
+          } else {
+            this.selectedValue = currentAnswer;
+          }
+        }
+      },
+      //倒计时
+      countTime() {
+        const vm = this,
+          endTime = vm.time * 60 * 1000 + vm.currentTime,
+          currentTime = new Date().getTime(),
+          restTime = endTime - currentTime,
+          hours = getNumberPrefix(parseInt(restTime / (1000 * 60 * 60) % 24, 10)),
+          minutes = getNumberPrefix(parseInt(restTime / (1000 * 60) % 60, 10)),
+          seconds = getNumberPrefix(parseInt(restTime / 1000 % 60, 10));
+        vm.restTime = `${hours}:${minutes}:${seconds}`;
+        vm.timer = setTimeout(function(){
+          if(restTime > 0){
+            vm.countTime();
+          }else if(restTime <= 0){
+            clearTimeout(vm.timer);
+            vm.$Message.warning("交卷时间已到，系统将帮您自动交卷");
+            setTimeout(() => {
+              vm.handleSubmit();
+            }, 2000)
+          }
+        }, 1000);
+      }
     }
-}
+  }
 </script>
 
 <style>
-.exam{
+  .exam{
     width: 100%;
     height: 100%;
     font-size: 1rem;
-}
-.exam .header,
-.exam section,
-.time-count-wrap
-{
+  }
+  .exam .header,
+  .exam section,
+  .time-count-wrap
+  {
     padding: 15px;
     text-align: left;
-}
-.exam .header
-{
+  }
+  .exam .header
+  {
     border-bottom: 9px solid #ccc;
-}
-.header .describe,
-.time-count-wrap
-{
+  }
+  .header .describe,
+  .time-count-wrap
+  {
     font-size: 0.8rem;
     color: rgba(135, 134, 136, 0.84);
-}
-.header .describe span:nth-child(2){
+  }
+  .header .describe span:nth-child(2){
     float: right;
-}
-.exam section{
+  }
+  .exam section{
     border-left: 5px solid red;
     background: url(../assets/3.jpg) 100% 100%;;
-}
-section .ivu-radio-group label{
+  }
+  section .ivu-radio-group label{
     display: block;
     padding: 7px 0px;
     white-space: normal;
-}
-.buttons-group{
+  }
+  section .ivu-checkbox-group label{
+    display: block;
+    padding: 7px 0px;
+    white-space: normal;
+  }
+  .buttons-group{
     padding: 30px 0px;
-}
-.buttons-group button{
+  }
+  .buttons-group button{
     padding-left: 25px;
     padding-right: 25px;
     margin-left: 5px;
-}
-.ivu-radio-wrapper{
+  }
+  .ivu-radio-wrapper{
     font-size: 0.9rem;
-}
-span.ivu-radio+*{
-  position: relative;
-  left: 20px;
-}
-.item-options .ivu-radio{
-  position: absolute!important;
-}
+  }
+  .ivu-checkbox-wrapper{
+    font-size: 0.9rem;
+  }
+  span.ivu-radio+*{
+    position: relative;
+    left: 20px;
+  }
+  span.ivu-checkbox+*{
+    position: relative;
+    left: 20px;
+  }
+  .item-options .ivu-radio{
+    position: absolute!important;
+  }
+  .item-options .ivu-checkbox{
+    position: absolute!important;
+  }
 </style>
--- src/page/result.vue
+++ src/page/result.vue
@@ -1,139 +1,162 @@
 <template>
   <div class="exam-result">
-      <div class="scores-container">
-          <div class="">
-            <header class="scores">{{getScore}}分</header>
-            <label class="result-tips">{{resultTips}}</label>
-          </div>
+    <div class="scores-container">
+      <div class="">
+        <header class="scores">{{getScore}}分</header>
+        <label class="result-tips">{{resultTips}}</label>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {mapState, mapMutations} from 'vuex'
+  import {mapState, mapMutations} from 'vuex'
 
-export default {
+  export default {
     data () {
-        return {
-            //答错的题
-            errorExam: [],
-            //答对的题
-            sureExam: [],
-            //总分
-            totalScore: 0,
-            //得分
-            getScore: 0,
-            resultTips: "",
-            examList: []
-        }
+      return {
+        //答错的题
+        errorExam: [],
+        //对部分题
+        halfExam: [],
+        //答对的题
+        sureExam: [],
+        //总分
+        totalScore: 0.0,
+        //得分
+        getScore: 0.0,
+        resultTips: "",
+        examList: []
+      }
     },
     computed:mapState([
-        'saveAnswer',
-        'useTime',
-        'examId'
+      'saveAnswer',
+      'useTime',
+      'examId'
     ]),
     mounted () {
-        const vm = this;
-        vm.examList = vm.$store.state.examList[this.examId - 1] || [];
-        vm.getSureAnswer();
+      const vm = this;
+      vm.examList = vm.$store.state.examList[this.examId - 1] || [];
+      vm.getSureAnswer();
     },
     methods: {
-        getSureAnswer() {
-            var vm = this;
-            // if(vm.examList.length !== vm.saveAnswer.length){
-            //     vm.$Message.error({
-            //         content: '系统错误，请稍后重试',
-            //         duration: 3
-            //     });
-            //     return;
-            // }
-            vm.examList.forEach(function(item, index){
-                vm.totalScore += parseInt(item.score);
-                if((item.answer + 1) == vm.saveAnswer[index]){
-                    vm.sureExam.push(item);
-                    vm.getScore += parseInt(item.score);
-                }else{
-                    vm.errorExam.push(item);
+      getSureAnswer() {
+        var vm = this;
+        // if(vm.examList.length !== vm.saveAnswer.length){
+        //     vm.$Message.error({
+        //         content: '系统错误，请稍后重试',
+        //         duration: 3
+        //     });
+        //     return;
+        // }
+        vm.examList.forEach(function(item, index){
+          vm.totalScore += parseInt(item.score);
+          if (Array.isArray(vm.saveAnswer[index]) && Array.isArray(item.answer)) {
+            if (vm.saveAnswer[index].length === item.answer.length
+              && vm.saveAnswer[index].every(function(v,i) { return v === item.answer[i]})) {  // 全部正确
+              vm.sureExam.push(item);
+              vm.getScore += item.score;
+            } else {
+              let errorFlag = false;
+              for (let i = 0; i < vm.saveAnswer[index].length; i++) {
+                if (item.answer.indexOf(vm.saveAnswer[index][i]) === -1) {
+                  errorFlag = true;
+                  break;
                 }
-            })
-            vm.setResultTips();
-            console.log(this.useTime)
-            console.log("答对题:" + vm.sureExam.length);
-        },
-        setResultTips() {
-            const scores = [100 ,90, 70, 60, 40, 20],
-                  tips = [
-                      '哇，你太优秀了，厉害了！！！',
-                      '智商只差一步就爆表了，继续加油！',
-                      '表现很好，good！',
-                      '嗯，不错，还有很大的进步空间！',
-                      '还需要继续加油哦！！！',
-                      'What are you弄啥嘞！'
-                  ];
-            if(this.getScore <= scores[5]) {
-                this.resultTips = tips[5];
-                return;
+              }
+              if (!errorFlag) {
+                vm.getScore += item.score/2;
+                vm.halfExam.push(item);
+              } else {
+                vm.errorExam.push(item);
+              }
             }
-            if(this.getScore <= scores[4]) {
-                this.resultTips = tips[4];
-                return;
+          } else {
+            if(item.answer === vm.saveAnswer[index]) {
+              vm.sureExam.push(item);
+              vm.getScore += item.score;
+            } else {
+              vm.errorExam.push(item);
             }
-            if(this.getScore <= scores[3]) {
-                this.resultTips = tips[3];
-                return;
-            }
-            if(this.getScore <= scores[2]) {
-                this.resultTips = tips[2];
-                return;
-            }
-            if(this.getScore <= scores[1]) {
-                this.resultTips = tips[1];
-                return;
-            }
-            if(this.getScore <= scores[0]) {
-                this.resultTips = tips[0];
-                return;
-            }
+          }
+        });
+
+        vm.setResultTips();
+        // console.log(this.useTime);
+        // console.log("答对题:" + vm.sureExam.length + "对一半题:" + vm.halfExam.length);
+      },
+      setResultTips() {
+        const scores = [100 ,90, 80, 60, 40, 20],
+          tips = [
+            '哇，为何你这么秀！！',
+            '表现超棒，继续加油！',
+            'good,还有很大空间！',
+            '请您再接再厉！！！',
+            '请继续加油亲！！！',
+            'What are you弄啥嘞！'
+          ];
+        if(this.getScore <= scores[5]) {
+          this.resultTips = tips[5];
+          return;
         }
+        if(this.getScore <= scores[4]) {
+          this.resultTips = tips[4];
+          return;
+        }
+        if(this.getScore <= scores[3]) {
+          this.resultTips = tips[3];
+          return;
+        }
+        if(this.getScore <= scores[2]) {
+          this.resultTips = tips[2];
+          return;
+        }
+        if(this.getScore <= scores[1]) {
+          this.resultTips = tips[1];
+          return;
+        }
+        if(this.getScore <= scores[0]) {
+          this.resultTips = tips[0];
+        }
+      }
     }
-}
+  }
 </script>
 
 <style scoped>
-    .exam-result{
-        height: 100%;
-        width: 100%;
-        background: url(../assets/4.jpg) no-repeat;
-        background-size: 100%;
-    }
-    .scores-container{
-        background: url(../assets/5.png) no-repeat;
-        background-size: 100%;
-        width: 14rem;
-        height: 14rem;
-        margin: 0 auto;
-        position: relative;
-    }
-    .scores-container .scores{
-        position: absolute;
-        width: 100%;
-        text-indent: 1.3rem;
-        top: 6.5rem;
-        font-size: 1.4rem;
-        font-weight: bolder;
-        color: #412318;
-        text-align: center;
-    }
-    .result-tips{
-        position: absolute;
-        width: 90%;
-        margin: 0 auto;
-        display: block;
-        top: 9rem;
-        color: #3e2415;
-        display: block;
-        font-size: 1.1rem;
-        left: 1rem;
-        text-align: center;
-    }
+  .exam-result{
+    height: 100%;
+    width: 100%;
+    background: url(../assets/4.jpg) no-repeat;
+    background-size: 100%;
+  }
+  .scores-container{
+    background: url(../assets/5.png) no-repeat;
+    background-size: 100%;
+    width: 14rem;
+    height: 14rem;
+    margin: 0 auto;
+    position: relative;
+  }
+  .scores-container .scores{
+    position: absolute;
+    width: 100%;
+    text-indent: 1.3rem;
+    top: 6.5rem;
+    font-size: 1.4rem;
+    font-weight: bolder;
+    color: #412318;
+    text-align: center;
+  }
+  .result-tips{
+    position: absolute;
+    width: 90%;
+    margin: 0 auto;
+    display: block;
+    top: 9rem;
+    color: #3e2415;
+    font-size: 1.1rem;
+    left: 1rem;
+    text-align: center;
+  }
 </style>
